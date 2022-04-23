1b049ad9be48ba513bb832ce10fe7b6c78e3c28e
Aryan296 <97389500+Aryan296@users.noreply.github.com>
2022-04-23 11:58:34 +0530

    Add files via upload

---
+++ skills.js
@@ -0,0 +1,26 @@
+// Enter all details Here
+var x,x1,x2,x3,x4,x5,x6,x7,x8,x9,e1,e2,e3;
+// Enters user name
+
+x="Aryan Kaushik"
+
+// Enter your skills
+
+x1="Ux/Ui Designer";
+x2="Adobe After effects";
+x3="3D designing";
+x4="3D Modelling";
+x5="Adobe Photoshop";
+x6="Adobe Illustrate";
+x7="Web Development";
+x8="UX Research";
+x9="Digital Designing";
+
+// enter education/ academic details
+
+// 12th school name
+e1="Strawberry Field";
+// graduation detail 
+e2="DePaul University";
+// post graduation detail
+e3="University of Pennsylvania";
